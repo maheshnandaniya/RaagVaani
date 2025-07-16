3446d34e388b9cf241b90e5ad0fc2ae9b59af441
Mahesh Nandaniya <76687572+maheshnandaniya@users.noreply.github.com>
2025-07-16 06:53:33 +0530

    Update bark_wrapper.py

--- musicgen_ai/bark_wrapper.py
+++ musicgen_ai/bark_wrapper.py
@@ -1,7 +1,7 @@
 from bark import SAMPLE_RATE, generate_audio
 import soundfile as sf
 
-def generate_bark_audio(lyrics_text, output_path="bark_output.wav"):
-    audio_array = generate_audio(lyrics_text)
+def generate_bark_audio(lyrics_text, output_path="bark_output.wav", speaker="v2/en_speaker_6"):
+    audio_array = generate_audio(lyrics_text, history_prompt=speaker)
     sf.write(output_path, audio_array, SAMPLE_RATE)
     return output_path
